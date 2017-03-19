119885641a93e529b56164aa739ff2a2d620a9d9
Ali Mousa <amousa11@gmail.com>
2017-03-19 16:12:41 -0700

    submit button updates table

--- smartcontractui/src/App.js
+++ smartcontractui/src/App.js
@@ -10,7 +10,7 @@ import FormContainer from './containers/FormContainer';
 var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 
 var smartContractABI = [{"constant":false,"inputs":[{"name":"contractId","type":"uint256"}],"name":"getBids","outputs":[{"name":"","type":"bytes32[]"},{"name":"","type":"address[]"},{"name":"","type":"uint256[]"},{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_asset","type":"bytes32"},{"name":"_quantity","type":"uint256"},{"name":"_targetPrice","type":"uint256"},{"name":"_targetTime","type":"uint256"}],"name":"addContract","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"contracts","outputs":[{"name":"contractId","type":"uint256"},{"name":"asset","type":"bytes32"},{"name":"quantity","type":"uint256"},{"name":"targetPrice","type":"uint256"},{"name":"targetTime","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getContracts","outputs":[{"name":"","type":"uint256[]"},{"name":"","type":"bytes32[]"},{"name":"","type":"uint256[]"},{"name":"","type":"uint256[]"},{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"cid","type":"uint256"},{"name":"_supplier","type":"bytes32"},{"name":"_price","type":"uint256"},{"name":"_bidTime","type":"uint256"}],"name":"bid","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]
-var smartContractAddress = '0xd178648ba2b0a5f19b2829569311e201d6b6ef90'
+var smartContractAddress = '0x7b0d0c14d2de3ffd5f59f4245026fc40098fae72'
 var smartContract = ETHEREUM_CLIENT.eth.contract(smartContractABI).at(smartContractAddress)
 
 class App extends Component {
@@ -47,7 +47,7 @@ class App extends Component {
           <td>{ETHEREUM_CLIENT.toDecimal(this.state.qty[index])}</td>
           <td>{ETHEREUM_CLIENT.toDecimal(this.state.tPrice[index])}</td>
           <td>{this.state.tTime[index]}</td>
-        </tr>            
+        </tr>
         )
     })
 
@@ -57,7 +57,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        
+
         <div className="Aligner">
           <div className="left">
             <div className="formLeft">
