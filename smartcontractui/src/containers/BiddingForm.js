e400bcd5605cea853ab84e975919d812088390cd
Collin Chin <collin.chin@berkeley.edu>
2017-03-22 18:43:14 -0700

    bidding form updated

--- smartcontractui/src/containers/BiddingForm.js
+++ smartcontractui/src/containers/BiddingForm.js
@@ -3,8 +3,6 @@ import SingleInput from '../components/SingleInput';
 import {ETHEREUM_CLIENT, smartContract} from '../components/EthereumSetup';
 // import BidTable from './BidTable';
 
-var contractBids = [];
-
 class BiddingForm extends Component {
 	constructor(props) {
 		super(props);
@@ -63,7 +61,6 @@ class BiddingForm extends Component {
 			thing4: this.state.thing4
 		};
 
-		contractBids.push(formPayload.thing1);
 		// uint cid, bytes32 _supplier, uint _price, uint _bidTime
 		smartContract.bid.sendTransaction(formPayload.thing1, formPayload.thing2, formPayload.thing3, formPayload.thing4, {from: ETHEREUM_CLIENT.eth.accounts[1], gas: 200000});
 
