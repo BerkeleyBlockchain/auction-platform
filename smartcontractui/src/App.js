23db2d2e2cf0cce9bb1ed8b270c2a961cd198649
Ali Mousa <amousa11@gmail.com>
2017-03-22 20:54:38 -0700

    uPort

--- smartcontractui/src/App.js
+++ smartcontractui/src/App.js
@@ -7,14 +7,31 @@ import FormContainer from './containers/FormContainer';
 import BiddingForm from './containers/BiddingForm';
 import BidTable from './containers/BidTable';
 import ContractTable from './containers/ContractTable';
+import { Connect } from 'uport-connect';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    };
+
+    componentWillMount() {
+        this.state = {
+            credential: 'null'
+        };
+        const uport = new Connect('Airbus');
+        uport.requestCredentials().then((credentials) => {
+            this.setState( {credential : credentials} );
+            console.log(credentials);
+        });
+    };
+
   render () {
     return (
       <div className="App">
 
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h3>Logged in {this.state.credential.name} as with address {this.state.credential.address}</h3>
         </div>
 
         <div className="Aligner">
@@ -24,7 +41,7 @@ class App extends Component {
                 <BiddingForm  />
               </div>
             </div>
-        </div> 
+        </div>
 
         <div className="right">
           <div className="formRight">
@@ -38,6 +55,7 @@ class App extends Component {
 
       </div>
     );
+
   }
 }
 
